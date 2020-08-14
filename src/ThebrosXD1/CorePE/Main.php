6a8a6e24c7b0ba34dd9325fb19009a99ce1a4b81
ThebrosXD <64794381+ThebrosXD@users.noreply.github.com>
2020-08-13 19:24:18 -0500

    Create Main.php

---
+++ src/ThebrosXD1/CorePE/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ThebrosXD1\CorePE;
+#Code Publico
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class CorePE extends PluginBase implements Listener {
+
+  public function OnLoad(){
+  $this->getLogger()->Info( Cargando CorePE)
+  
+  public function OnEnable(){
+  $this->getLogger()->Info( CorePE Habilitado)
+  
+  Public function OnDisabled(){
+  $thid->getLogger()->Info( CorePE Desabilitado)
